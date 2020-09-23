f88c961f3541049a47edf4bb499f4339aead8a68
Ryan K Burns <ryankburns@gmail.com>
2020-09-23 04:54:10 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,11 +4,9 @@ def hello_t(array)
     yeild array[i]
     i = i + 1
   end
-end
-array = ["Tim", "Tom", "Jim"]
-# call your method here!
-def hello_t(array) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
+  hello_t(array) do |name|
+    if name.start_with?("T")
+      puts "Hi, #{name}"
+    end
   end
 end
